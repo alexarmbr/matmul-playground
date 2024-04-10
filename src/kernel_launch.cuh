5f41c26e8b4730f71d142a78724160739b7bdcaf
alex armbruster <aaa370@georgetown.edu>
2024-04-10 11:58:16 -0600

    kernel 3 in progress

--- src/kernel_launch.cuh
+++ src/kernel_launch.cuh
@@ -2,6 +2,7 @@
 #include "host_utils.cuh"
 #include "kernels/tensorcore_1.cuh"
 #include "kernels/tensorcore_2.cuh"
+#include "kernels/tensorcore_3.cuh"
 #include "kernels/fp32_blocktiling.cuh"
 
 #include <cublas_v2.h>
@@ -84,7 +85,7 @@ void tensorcore_2_launch(sgemm_params<half> device_sgemm_params, KernelLogger& t
     constexpr unsigned int WARPS_PER_BLOCK_K = 2;
     constexpr unsigned int BM_dim = WM_dim * WARPS_PER_BLOCK_M;
     constexpr unsigned int BN_dim = WN_dim * WARPS_PER_BLOCK_N;
-    constexpr unsigned int BK_dim = WK_dim * WARPS_PER_BLOCK_K; 
+    constexpr unsigned int BK_dim = WK_dim * WARPS_PER_BLOCK_K;
     const unsigned int M = device_sgemm_params.M;
     const unsigned int N = device_sgemm_params.N;
     const unsigned int K = device_sgemm_params.K;
@@ -144,6 +145,95 @@ void tensorcore_2_launch(sgemm_params<half> device_sgemm_params, KernelLogger& t
     }
 
 }
+
+
+void tensorcore_3_launch(sgemm_params<half> device_sgemm_params, KernelLogger& timer, const unsigned int num_runs = 10)
+{
+    constexpr unsigned int WM_dim = 16;
+    constexpr unsigned int WN_dim = 16;
+    constexpr unsigned int WK_dim = 16;
+
+    constexpr unsigned int MMA_TILES_PER_WARP_TILE_M=1;
+    constexpr unsigned int MMA_TILES_PER_WARP_TILE_N=1;
+    constexpr unsigned int MMA_TILES_PER_WARP_TILE_K=1;
+
+    constexpr unsigned int WARP_TILES_PER_BLOCK_M = 1;
+    constexpr unsigned int WARP_TILES_PER_BLOCK_N = 1;
+    constexpr unsigned int WARP_TILES_PER_BLOCK_K = 1; // is this needed?
+
+    constexpr unsigned int BM_dim = WM_dim * WARP_TILES_PER_BLOCK_M * MMA_TILES_PER_WARP_TILE_M;
+    constexpr unsigned int BN_dim = WN_dim * WARP_TILES_PER_BLOCK_N * MMA_TILES_PER_WARP_TILE_N;
+    constexpr unsigned int BK_dim = WK_dim * WARP_TILES_PER_BLOCK_K * MMA_TILES_PER_WARP_TILE_K;
+
+    const unsigned int M = device_sgemm_params.M;
+    const unsigned int N = device_sgemm_params.N;
+    const unsigned int K = device_sgemm_params.K;
+
+    assert(M % BM_dim == 0);
+    assert(N % BN_dim == 0);
+    assert(K % BK_dim == 0);
+    
+    constexpr unsigned int WARP_SIZE = 32;
+    const unsigned int BlocksM = M / BM_dim;
+    const unsigned int BlocksN = N / BN_dim;
+    const unsigned int ThreadsM = 1;
+    const unsigned int ThreadsN = WARP_SIZE * WARP_TILES_PER_BLOCK_M * WARP_TILES_PER_BLOCK_N;
+
+    dim3 gridDim(BlocksN, BlocksM);
+    dim3 blockDim(ThreadsN, ThreadsM);
+
+    tensorcore_3
+    <BM_dim, BN_dim, BK_dim, WM_dim, WN_dim, WK_dim,
+    MMA_TILES_PER_WARP_TILE_M, MMA_TILES_PER_WARP_TILE_N, MMA_TILES_PER_WARP_TILE_K>
+    <<<gridDim, blockDim>>>(
+        device_sgemm_params.A,
+        device_sgemm_params.B,
+        device_sgemm_params.C,
+        device_sgemm_params.D,
+        device_sgemm_params.alpha,
+        device_sgemm_params.beta,
+        M,
+        N,
+        K
+    );
+    CUDA_CHECK(cudaDeviceSynchronize());
+    CUDA_CHECK(cudaPeekAtLastError());
+
+    if (num_runs != 0)
+    {
+        for (int i = 0; i < num_runs; i++)
+        {
+            timer.Start();
+            tensorcore_3
+            <BM_dim, BN_dim, BK_dim, WM_dim, WN_dim, WK_dim,
+            MMA_TILES_PER_WARP_TILE_M, MMA_TILES_PER_WARP_TILE_N, MMA_TILES_PER_WARP_TILE_K>
+            <<<gridDim, blockDim>>>(
+                device_sgemm_params.A,
+                device_sgemm_params.B,
+                device_sgemm_params.C,
+                device_sgemm_params.D,
+                device_sgemm_params.alpha,
+                device_sgemm_params.beta,
+                M,
+                N,
+                K
+            );
+            timer.Stop();
+        }
+        double gflops_per_sec = timer.logKernelStats(M, N, K);
+        std::cout << "Naive TensorCore: " << gflops_per_sec << " GFLOPS/sec for " << M << "x" << N << "x" << K << std::endl;
+        CUDA_CHECK(cudaPeekAtLastError());
+    }
+
+}
+
+
+
+
+
+
+
+
 
 
 void cublas_fp16_launch(sgemm_params<half> device_sgemm_params, KernelLogger& timer)
---
+++ src/kernels/tensorcore_3.cuh
@@ -0,0 +1,143 @@
+#pragma once
+#include <cuda.h>
+#include <mma.h>
+
+#include "device_utils.cuh"
+
+using namespace nvcuda;
+
+template <unsigned int BM_dim,
+unsigned int BN_dim,
+unsigned int BK_dim,
+unsigned int WM_dim,
+unsigned int WN_dim,
+unsigned int WK_dim,
+unsigned int MMA_M_dim,
+unsigned int MMA_N_dim,
+unsigned int MMA_K_dim
+>
+__global__ void tensorcore_3(half* A,
+  half* B,
+  half* C,
+  half* D,
+  const float alpha,
+  const float beta,
+  const unsigned int M,
+  const unsigned int N,
+  unsigned int K)
+{
+  static_assert(BM_dim % WM_dim == 0);
+  static_assert(BN_dim % WN_dim == 0);
+  constexpr unsigned int WARPS_PER_BLOCK_N = BN_dim / WN_dim;
+  constexpr unsigned int WARP_SIZE = 32;
+  const unsigned int A_stride = K;
+  const unsigned int B_stride = N;
+  const unsigned int CD_stride = N;
+  
+  // top left coords of the block tile
+  const unsigned int block_m = blockIdx.y * BM_dim;
+  const unsigned int block_n = blockIdx.x * BN_dim;
+  
+  // top left coords of a warp tile, relative to the block tile its in
+  const unsigned int warp_index = threadIdx.x / WARP_SIZE;
+  const unsigned int warp_tile_m = warp_index / WARPS_PER_BLOCK_N;
+  const unsigned int warp_tile_n = warp_index - (warp_tile_m * WARPS_PER_BLOCK_N);
+  const unsigned int warp_m = warp_tile_m * WM_dim;
+  const unsigned int warp_n = warp_tile_n * WN_dim;
+
+  // declare shared memory tiles for caching matrix tiles at block level
+  __shared__ half A_shmem_blocktile[BM_dim * BK_dim];
+  __shared__ half B_shmem_blocktile[BK_dim * BN_dim];
+  __shared__ half CD_shmem_blocktile[BM_dim * BN_dim];
+  constexpr unsigned int A_shmem_stride = BK_dim;
+  constexpr unsigned int B_shmem_stride = BN_dim;
+  constexpr unsigned int CD_shmem_stride = BN_dim;
+
+  // declare wmma fragments for caching matrix fragments at warp level
+  // each warp computes MMA_TILES_M * MMA_TILES_N tiles of size WM * WN in the output
+  static_assert(WM_dim % MMA_M_dim == 0);
+  static_assert(WN_dim % MMA_N_dim == 0);
+  static_assert(WK_dim % MMA_K_dim == 0);
+  constexpr unsigned int MMA_TILES_M = WM_dim / MMA_M_dim;
+  constexpr unsigned int MMA_TILES_N = WN_dim / MMA_N_dim;
+  constexpr unsigned int MMA_TILES_K = WK_dim / MMA_K_dim;
+  wmma::fragment<wmma::matrix_a, WM_dim, WN_dim, WK_dim, half, wmma::row_major> a_frag;
+  wmma::fragment<wmma::matrix_b, WM_dim, WN_dim, WK_dim, half, wmma::row_major> b_frag[MMA_TILES_K][MMA_TILES_N];
+  wmma::fragment<wmma::accumulator, WM_dim, WN_dim, WK_dim, half> c_frag;
+  wmma::fragment<wmma::accumulator, WM_dim, WN_dim, WK_dim, float> acc_frag[MMA_TILES_M][MMA_TILES_N];
+
+  // load tile of C into shared memory ahead of time
+  tileMemcpy<BM_dim, BN_dim, half>(C + block_m * CD_stride + block_n, CD_shmem_blocktile, CD_stride, CD_shmem_stride);
+  
+  // loop over shared memory block tiles along K dimension
+  for (unsigned int block_k = 0; block_k < K; block_k += BK_dim)
+  {
+    // load in current block tile of A,B along K dimension into shared memory
+    tileMemcpy<BM_dim, BK_dim, half>(A + block_m * A_stride + block_k, A_shmem_blocktile, A_stride, A_shmem_stride);
+    tileMemcpy<BK_dim, BN_dim, half>(B + block_k * B_stride + block_n, B_shmem_blocktile, B_stride, B_shmem_stride);
+    
+    __syncthreads();
+
+    // warps move along k dimension within the block tile
+    for (unsigned int warp_k = 0; warp_k < BK_dim; warp_k += WK_dim)
+    {
+      const unsigned int A_warp_index = warp_m * A_shmem_stride + warp_k;
+      const unsigned int B_warp_index = warp_k * B_shmem_stride + warp_n;
+
+      // each warp preloads the tiles of B that fall inside its warp tile
+      for (unsigned int tile_k = 0; tile_k < MMA_TILES_K; tile_k++)
+      {
+        for (unsigned int tile_n = 0; tile_n < MMA_TILES_N; tile_n++)
+        {
+          const unsigned int B_tile_index = B_warp_index + (tile_k * MMA_K_dim * B_shmem_stride) + (tile_n * MMA_N_dim);
+          wmma::load_matrix_sync(b_frag[tile_k][tile_n], B_shmem_blocktile + B_tile_index, B_shmem_stride);
+        }
+      }
+
+      
+      // outer product between mma tiles of B (already cached in registers by this point)
+      // and tiles of A which we load from shmem
+      // looping over tiles of A first allows us to minimize shmem traffic, each warp
+      // loads each tile of A from shared memory only once
+      for (unsigned int tile_k = 0; tile_k < MMA_TILES_K; tile_k++)
+      {
+        for (unsigned int tile_m = 0; tile_m < MMA_TILES_M; tile_m++)
+        {
+          const unsigned int A_tile_index = A_warp_index + (tile_m * MMA_M_dim * A_shmem_stride) + (tile_k * MMA_K_dim);
+          wmma::load_matrix_sync(a_frag, A_shmem_blocktile + A_tile_index, A_shmem_stride);
+          for (unsigned int tile_n = 0; tile_n < MMA_TILES_N; tile_n++)
+          {
+            wmma::mma_sync(acc_frag[tile_m][tile_n], a_frag, b_frag[tile_k][tile_n], acc_frag[tile_m][tile_n]);
+          }
+        }
+      }
+
+    }
+    __syncthreads();
+  }
+  
+  // loop over tiles of C,D and accumulate the final result from C, acc_frag into D
+  // const unsigned int CD_warp_index = warp_m * CD_shmem_stride + warp_n;
+  // for (unsigned int tile_m = 0; tile_m < MMA_TILES_M; tile_m++)
+  // {
+  //   for (unsigned int tile_n = 0; tile_n < MMA_TILES_N; tile_n++)
+  //   {
+  //     const unsigned int CD_tile_index = CD_warp_index + (tile_m * MMA_TILES_M * CD_shmem_stride) + (tile_n * MMA_N_dim);
+  //     wmma::load_matrix_sync(c_frag, CD_shmem_blocktile + CD_tile_index, CD_shmem_stride);
+
+  //   }
+  // }
+  
+  // wmma::load_matrix_sync(c_frag, CD_shmem_blocktile + CD_tile_index, CD_shmem_stride, wmma::mem_row_major);
+  // const half beta_ = (half) beta;
+  // for (int i = 0; i < c_frag.num_elements; i++)
+  // {
+  //   c_frag.x[i] = alpha * acc_frag.x[i] + (float) (beta_ * c_frag.x[i]);
+  // }
+  
+  // wmma::store_matrix_sync(CD_shmem_blocktile + CD_tile_index, c_frag, CD_shmem_stride, wmma::mem_row_major);
+  __syncthreads();
+  tileMemcpy<BM_dim, BN_dim, half>(CD_shmem_blocktile, D + block_m * CD_stride + block_n, CD_shmem_stride, CD_stride);
+}
+
+
