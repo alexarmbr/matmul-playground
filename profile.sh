6dc17a87fce76e0d55deb055359da3188c3de597
aarmbruster <a14armbr@gmail.com>
2024-08-04 23:24:44 +0000

    done

--- profile.sh
+++ profile.sh
@@ -1,12 +1,77 @@
+
+echo "Matrix size 1024x1024"
 echo "Kernel 1"
-./build/runner 1 1
+./build/runner 1 50 1024 1024 1024
 echo "Kernel 2"
-./build/runner 2 1
+./build/runner 2 50 1024 1024 1024
 echo "Kernel 3"
-./build/runner 3 1
+./build/runner 3 50 1024 1024 1024
 echo "Kernel 4"
-./build/runner 4 1
+./build/runner 4 50 1024 1024 1024
 echo "Kernel 5"
-./build/runner 5 1
+./build/runner 5 50 1024 1024 1024
 echo "Kernel 6"
-./build/runner 6 1
+./build/runner 6 50 1024 1024 1024
+echo "Kernel 7"
+./build/runner 7 50 1024 1024 1024
+echo "cuBLAS HGEMM"
+./build/runner 99 50 1024 1024 1024
+echo "------------------------------"
+
+echo "Matrix size 2048x2048"
+echo "Kernel 1"
+./build/runner 1 50 2048 2048 2048
+echo "Kernel 2"
+./build/runner 2 50 2048 2048 2048
+echo "Kernel 3"
+./build/runner 3 50 2048 2048 2048
+echo "Kernel 4"
+./build/runner 4 50 2048 2048 2048
+echo "Kernel 5"
+./build/runner 5 50 2048 2048 2048
+echo "Kernel 6"
+./build/runner 6 50 2048 2048 2048
+echo "Kernel 7"
+./build/runner 7 50 2048 2048 2048
+echo "cuBLAS HGEMM"
+./build/runner 99 50 2048 2048 2048
+echo "------------------------------"
+
+echo "Matrix size 4096x4096"
+echo "Kernel 1"
+./build/runner 1 50 4096 4096 4096
+echo "Kernel 2"
+./build/runner 2 50 4096 4096 4096
+echo "Kernel 3"
+./build/runner 3 50 4096 4096 4096
+echo "Kernel 4"
+./build/runner 4 50 4096 4096 4096
+echo "Kernel 5"
+./build/runner 5 50 4096 4096 4096
+echo "Kernel 6"
+./build/runner 6 50 4096 4096 4096
+echo "Kernel 7"
+./build/runner 7 50 4096 4096 4096
+echo "cuBLAS HGEMM"
+./build/runner 99 50 4096 4096 4096
+echo "------------------------------"
+
+
+echo "Matrix size 8192x8192"
+echo "Kernel 1"
+./build/runner 1 50 8192 8192 8192
+echo "Kernel 2"
+./build/runner 2 50 8192 8192 8192
+echo "Kernel 3"
+./build/runner 3 50 8192 8192 8192
+echo "Kernel 4"
+./build/runner 4 50 8192 8192 8192
+echo "Kernel 5"
+./build/runner 5 50 8192 8192 8192
+echo "Kernel 6"
+./build/runner 6 50 8192 8192 8192
+echo "Kernel 7"
+./build/runner 7 50 8192 8192 8192
+echo "cuBLAS HGEMM"
+./build/runner 99 50 8192 8192 8192
+echo "------------------------------"
--- src/main.cu
+++ src/main.cu
@@ -1,67 +1,59 @@
 #include <assert.h>
 #include "host_utils.cuh"
 
-    // void kernel_1_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
-    // void kernel_2_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
-    // void kernel_3_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
-    // void kernel_4_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
-    // void kernel_5_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
-    // void kernel_6_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
+    void kernel_1_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
+    void kernel_2_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
+    void kernel_3_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
+    void kernel_4_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
+    void kernel_5_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
+    void kernel_6_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
     void kernel_7_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
     void cublas_launch(sgemm_params device_sgemm_params, KernelLogger& timer, const unsigned int num_runs);
 
   int main(int argc, char **argv) {
     bool check_on_cpu = false;
     
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <kernel_id> <num_iterations>" << std::endl;
+    if (argc != 6) {
+        std::cerr << "Usage: " << argv[0] << " <kernel_id> <num_iterations> <M> <N> <K>" << std::endl;
         return 1;
     }
 
     const unsigned int kernel_id = std::stoi(argv[1]);
     std::string timer_name = "kernel_" + std::to_string(kernel_id);
     const unsigned int num_iterations = std::stoi(argv[2]);
+    const unsigned int M = std::stoi(argv[3]);
+    const unsigned int N = std::stoi(argv[4]);
+    const unsigned int K = std::stoi(argv[5]);
     assert(num_iterations > 0);
 
     KernelLogger timer(timer_name);
-    // const unsigned int M = 256;
-    // const unsigned int N = 256;
-    // const unsigned int K = 256;
-    const unsigned int M = 4096;
-    const unsigned int N = 4096;
-    const unsigned int K = 4096;
-    
+
     auto [device_sgemm_params, host_sgemm_params] = sgemm_setup(M, N, K);
-    // device_sgemm_params.alpha = 0.0;
-    // host_sgemm_params.alpha = 0.0;
-    // device_sgemm_params.beta = 1.0;
-    // host_sgemm_params.beta = 1.0;
     switch (kernel_id) {
-        // case 1:
-        //     kernel_1_launch(device_sgemm_params, timer, num_iterations);
-        //     break;
-        // case 2:
-        //     kernel_2_launch(device_sgemm_params, timer, num_iterations);
-        //     break;
-        // case 3:
-        //     kernel_3_launch(device_sgemm_params, timer, num_iterations);
-        //     break;
-        // case 4:
-        //     kernel_4_launch(device_sgemm_params, timer, num_iterations);
-        //     break;
-        // case 5:
-        //     kernel_5_launch(device_sgemm_params, timer, num_iterations);
-        //     break;
-        // case 6:
-        //     kernel_6_launch(device_sgemm_params, timer, num_iterations);
-        //     break;
+        case 1:
+            kernel_1_launch(device_sgemm_params, timer, num_iterations);
+            break;
+        case 2:
+            kernel_2_launch(device_sgemm_params, timer, num_iterations);
+            break;
+        case 3:
+            kernel_3_launch(device_sgemm_params, timer, num_iterations);
+            break;
+        case 4:
+            kernel_4_launch(device_sgemm_params, timer, num_iterations);
+            break;
+        case 5:
+            kernel_5_launch(device_sgemm_params, timer, num_iterations);
+            break;
+        case 6:
+            kernel_6_launch(device_sgemm_params, timer, num_iterations);
+            break;
         case 7:
             kernel_7_launch(device_sgemm_params, timer, num_iterations);
             break;
         case 99:
             cublas_launch(device_sgemm_params, timer, num_iterations);
             break;
-
     }
     
     if (check_on_cpu) {
